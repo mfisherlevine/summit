d7c817d17d88333f37650e8c6d153af02b7f774b
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2020-01-23 17:49:08 -0600

    Move package setup to stack.sh

--- stack.sh
+++ stack.sh
@@ -1,4 +1,6 @@
-
+setup lsst_distrib -t w_2020_03
 
 setup -j atmospec -r ~/repos/atmospec
 setup -j rapid_analysis -r ~/repos/rapid_analysis
+setup -j obs_base -r ~/repos/obs_base
+setup -j obs_lsst -r ~/repos/obs_lsst
