9b5ba5d031ae48632a3a0ef3d058db9ca7b18894
Merlin Fisher-Levine <merlin.fisherlevine@gmail.com>
2020-01-28 15:52:24 -0600

    Move to latest daily

--- stack.sh
+++ stack.sh
@@ -1,4 +1,6 @@
-setup lsst_distrib -t w_2020_04
+# setup lsst_distrib -t w_2020_04
+setup lsst_distrib -t d_2020_01_27
+
 
 setup -j atmospec -r $HOME/repos/atmospec
 setup -j rapid_analysis -r $HOME/repos/rapid_analysis
